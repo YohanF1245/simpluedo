11d70285eb71601ff8d8fb15a0842bdcf9692234
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-10 18:48:08 +0100

    feat(EntityCharacters.java): add character model

--- src/main/java/entity/EntityCharacters.java
+++ src/main/java/entity/EntityCharacters.java
@@ -1,1 +1,19 @@
+package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "personnages")
+public class EntityCharacters {
+	@Id
+	private Integer id_perso;
+	private String nom_perso;
+}
