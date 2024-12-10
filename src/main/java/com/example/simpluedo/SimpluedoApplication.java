668a3a61265fbf5af79fdf4b5e9c9bbd6565b8ec
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-10 18:45:19 +0100

    chore(repo): init spring boot 4 repo

---
+++ src/main/java/com/example/simpluedo/SimpluedoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpluedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpluedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpluedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpluedo/SimpluedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpluedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpluedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
