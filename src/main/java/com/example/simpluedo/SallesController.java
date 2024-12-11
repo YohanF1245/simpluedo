f858333311ea223e8005bb1749417d95a7f7581a
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-11 19:56:59 +0100

    feat(SallesController): add delete entry logic

--- src/main/java/com/example/simpluedo/SallesController.java
+++ src/main/java/com/example/simpluedo/SallesController.java
@@ -1,9 +1,12 @@
 package com.example.simpluedo;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,10 +34,20 @@ public class SallesController {
 	@PutMapping("/salles/{id}")
 	public ResponseEntity<Salles> updateSalles(@PathVariable int id, @RequestBody Salles salleInfos){
 		Salles salle = sallesRepo.findById(id)
-				.orElseThrow(()-> new RuntimeException("Error encountered!"));
+				.orElseThrow(()-> new RuntimeException("An error has occured!"));
 		salle.setId_salle(salleInfos.getId_salle());
 		salle.setNom_salle(salleInfos.getNom_salle());
 		Salles updatedSalles = sallesRepo.save(salle);
 		return ResponseEntity.ok(updatedSalles);
-}
 	}
+	@DeleteMapping("/salles/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable int id){
+		Salles salle = sallesRepo.findById(id)
+				.orElseThrow(()-> new RuntimeException("An error has occured") );
+		sallesRepo.delete(salle);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		
+		return ResponseEntity.ok(response);
+	}
+}
