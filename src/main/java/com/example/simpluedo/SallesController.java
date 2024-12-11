735f115587e94c66811516ca3cfa7356ed2d5aec
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-11 15:04:20 +0100

    feat(SallesController): add create post logic

--- src/main/java/com/example/simpluedo/SallesController.java
+++ src/main/java/com/example/simpluedo/SallesController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +19,11 @@ public class SallesController {
 	public List<Salles> getAllSalles(){
 		return sallesRepo.findAll();
 	}
+	
+	@PostMapping("/salles")
+	public Salles createSalles(@RequestBody Salles salle) {
+		return sallesRepo.save(salle);
+	}
+	
+	
 }
