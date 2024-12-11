a7aac17a36a247d059ec3bd2d49a2e3b7bf9aab3
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-11 19:44:36 +0100

    feat(SallesController): add updte entry logic

--- src/main/java/com/example/simpluedo/SallesController.java
+++ src/main/java/com/example/simpluedo/SallesController.java
@@ -3,8 +3,11 @@ package com.example.simpluedo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,13 @@ public class SallesController {
 		return sallesRepo.save(salle);
 	}
 	
-	
+	@PutMapping("/salles/{id}")
+	public ResponseEntity<Salles> updateSalles(@PathVariable int id, @RequestBody Salles salleInfos){
+		Salles salle = sallesRepo.findById(id)
+				.orElseThrow(()-> new RuntimeException("Error encountered!"));
+		salle.setId_salle(salleInfos.getId_salle());
+		salle.setNom_salle(salleInfos.getNom_salle());
+		Salles updatedSalles = sallesRepo.save(salle);
+		return ResponseEntity.ok(updatedSalles);
 }
+	}
