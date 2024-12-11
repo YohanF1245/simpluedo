250d85004dbb1c177d060e2abbf62a98b62396bd
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-12-11 20:25:21 +0100

    docs(SallesRepository): add javadoc

--- src/main/java/com/example/simpluedo/SallesRepository.java
+++ src/main/java/com/example/simpluedo/SallesRepository.java
@@ -2,7 +2,23 @@ package com.example.simpluedo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/**
+ * Interface SallesRepository<br>
+ * 
+ * This interface extends {@link JpaRepository} and gives CRUD operations functionality<br>
+ * 
+ * Usable methods : 
+ * <ul>
+ * 		<li>{@code save(S entity)}: create or update an entity</li>
+ * 		<li>{@code findById(int id)}: finds an entity by his id</li>
+ * 		<li>{@code findAll()}: finds all entities</li>
+ * 		<li>{@code deleteById(int id)}: delete an entity by his id</li>
+ * 		<li>{@code existsById(int id)}: check if an entity exist with this id</li>
+ * </ul>
+ * 
+ * @see JpaRepository
+ * @see Salles
+ */
 
 public interface SallesRepository extends JpaRepository<Salles, Integer>{
 
